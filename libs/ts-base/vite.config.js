c890c2732824f850e1b5dd9dd6d4fc30fd5621ab
Franz Sittampalam <shedali@gmail.com>
2023-01-22 22:46:09 +0000

    replace import meta vitest

--- libs/ts-base/vite.config.js
+++ libs/ts-base/vite.config.js
@@ -1,9 +1,12 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {
-     includeSource: ['src/**/*.{js,ts}'],
+  define: {
+    "import.meta.vitest": "undefined",
   },
-})
+  test: {
+    includeSource: ["src/**/*.{js,ts}"],
+  },
+});
